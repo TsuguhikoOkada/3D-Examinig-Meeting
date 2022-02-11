e12f39eccf71a24af5c5d966ffb6efdb9f7145b1
TsuguhikoOkada <tsugu.pc.academy@gmail.com>
2022-02-11 17:08:24 +0900

    Added the code for the gimmick click process.

---
+++ Assets/Scripts/Gimmicks/SampleGimmick.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleGimmick : MonoBehaviour
+{
+    // アイテムを持っている状態でクリックすると消える(クリック判定、アイテム所持判定)
+
+    public void OnClickObj()
+    {
+        Debug.Log("クリックしました！");
+    }
+}
