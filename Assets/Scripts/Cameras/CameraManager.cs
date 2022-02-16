8610ce5a91f28ac3326d91e74477c486f6f352d7
TsuguhikoOkada <tsugu.pc.academy@gmail.com>
2022-02-17 03:46:50 +0900

    Added all the essential parts to the production.

--- Assets/Scripts/Cameras/CameraManager.cs
+++ Assets/Scripts/Cameras/CameraManager.cs
@@ -8,22 +8,83 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
+    [SerializeField] GameObject _leftArrowObj;
+    [SerializeField] GameObject _rightArrowObj;
+    [SerializeField] GameObject _backArrowObj;
+
     // どこにどのカメラを有効にするか?
     // Mainの全体を回転するカメラ
     // ズーム表示時のカメラ
+    Camera _carrentCamera;
+    // 保持するカメラ
+    Camera _mainCamera;
     // Move時のカメラ
 
     // 全体カメラのポジションを作成
-    [SerializeField, Header("回転カメラポジション")] Transform[] _mainCameraPosition = default;
-    // Start is called before the first frame update
+    [SerializeField, Header("回転カメラポジション")] Transform[] _mainCameraTransforms ;
+
+    int _currentMainPosition;
+
+    /// <summary>CameraManagerのインスタンス</summary>///
+    public static CameraManager instance;
+
+    void Awake()
+    {
+        instance = this;
+    }
     void Start()
     {
-        Camera.main.transform.position = _mainCameraPosition[0].position;
+        _backArrowObj.SetActive(false);
+        _mainCamera = Camera.main;
+        _carrentCamera = Camera.main;
+        _currentMainPosition = 0;
+        _carrentCamera.transform.position = _mainCameraTransforms[_currentMainPosition].position;
+        _carrentCamera.transform.rotation = _mainCameraTransforms[_currentMainPosition].rotation;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void TurnLeft()
     {
-        
+        _currentMainPosition--;
+
+        if (_currentMainPosition < 0)
+        {
+            _currentMainPosition = _mainCameraTransforms.Length - 1;
+        }
+
+        _carrentCamera.transform.position = _mainCameraTransforms[_currentMainPosition].position;
+        _carrentCamera.transform.rotation = _mainCameraTransforms[_currentMainPosition].rotation;
+    }
+
+    public void TurnRight()
+    {
+        _currentMainPosition++;
+
+        if (_currentMainPosition >= _mainCameraTransforms.Length)
+        {
+            _currentMainPosition = 0;
+        }
+
+        _carrentCamera.transform.position = _mainCameraTransforms[_currentMainPosition].position;
+        _carrentCamera.transform.rotation = _mainCameraTransforms[_currentMainPosition].rotation;
+    }
+
+    public void SetZoomCamera(Camera camera)
+    {
+        _carrentCamera.gameObject.SetActive(false);
+        camera.gameObject.SetActive(true);
+        _carrentCamera = camera;
+        _backArrowObj.SetActive(true);
+        _leftArrowObj.SetActive(false);
+        _rightArrowObj.SetActive(false);
+    }
+
+    public void UnZoomButton()
+    {
+        _mainCamera.gameObject.SetActive(true);
+        _carrentCamera.gameObject.SetActive(false);
+        _carrentCamera = _mainCamera;
+        _backArrowObj.SetActive(false);
+        _leftArrowObj.SetActive(true);
+        _rightArrowObj.SetActive(true);
     }
 }
---
+++ Assets/Scripts/Cameras/CameraZoomObj.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoomObj : MonoBehaviour
+{
+    /// <summary>ズームカメラの変数</summary>///
+    [SerializeField, Header("ズームカメラ")] Camera _zoomCamera;
+
+    // クリックしたらズーム位置を記録したカメラに切り替える
+    public void OnClickThis()
+    {
+        Debug.Log("カメラ切り替え");
+        CameraManager.instance.SetZoomCamera(_zoomCamera);
+    }
+}
